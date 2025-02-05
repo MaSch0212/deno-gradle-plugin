6d12ada7003f7f4b8912ee44fe13610a6af4ab53
Marc Schmidt <code@masch212.de>
2025-02-05 22:24:52 +0100

    chore: local publish for now

--- gradle-plugin/build.gradle.kts
+++ gradle-plugin/build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
     `kotlin-dsl`
     `java-gradle-plugin`
+    `java-library`
     `maven-publish`
-    id("com.gradle.plugin-publish") version "1.3.1"
 }
 
 group = "io.github.masch0212"
@@ -37,7 +37,17 @@ dependencies {
 
 publishing {
     repositories {
-        mavenCentral()
+//        maven {
+//            name = "OSSRH"
+//            url = uri("https://oss.sonatype.org/service/local/staging/deploy/maven2/")
+//            credentials {
+//                username = System.getenv("MAVEN_USERNAME") ?: project.property("MAVEN_USERNAME").toString()
+//                password = System.getenv("MAVEN_PASSWORD") ?: project.property("MAVEN_PASSWORD").toString()
+//            }
+//        }
+        maven {
+            url = uri("file://${project.buildDir}/repo")
+        }
     }
     publications {
         create<MavenPublication>("maven") {
@@ -45,6 +55,33 @@ publishing {
             groupId = "io.github.masch0212"
             artifactId = "deno-gradle-plugin"
             version = "0.0.1"
+
+            pom {
+                name.set("Deno Gradle Plugin")
+                description.set("A plugin to install and manage Deno versions")
+                url.set("https://github.com/MaSch0212/deno-gradle-plugin")
+
+                scm {
+                    url.set("https://github.com/MaSch0212/deno-gradle-plugin")
+                    connection.set("scm:git:git://github.com/MaSch0212/deno-gradle-plugin.git")
+                    developerConnection.set("scm:git:ssh://github.com/MaSch0212/deno-gradle-plugin.git")
+                }
+
+                licenses {
+                    license {
+                        name.set("MIT License")
+                        url.set("https://opensource.org/licenses/MIT")
+                    }
+                }
+
+                developers {
+                    developer {
+                        id.set("MaSch0212")
+                        name.set("Marc Schmidt")
+                        email.set("code@masch212.de")
+                    }
+                }
+            }
         }
     }
 }
